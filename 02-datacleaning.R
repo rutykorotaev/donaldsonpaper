f323dba1455150c36531675608c45b029319f044
rutykorotaev@mail.utoronto.ca <ruty.korotaiev@mail.utoronto.ca>
2023-02-02 00:06:05 -0500

    Cleaned and developed datasets based on housing status and demographics.

--- 02-datacleaning.R
+++ 02-datacleaning.R
@@ -1,6 +1,14 @@
-data <- read_csv("inputs/rawdata.csv")
+#Download dataset
+data1 <- read.csv("inputs/rawdata.csv")
 
-data <- 
-  data |>
+#Filter data by housing status of refugees
+Housing_Status <-
+  data1 |>
   filter(population_group == "Refugees") %>% 
-  select(actively_homeless, `date(mmm-yy)`)
+  select(date.mmm.yy., actively_homeless, newly_identified, moved_to_housing, became_inactive, returned_from_housing)
+
+#Filter data by gender and age of refugees
+Demographic_Data <-
+  data |>
+  filter(population_group == "Refugees") %>%
+  select(`date(mmm-yy)`, 'ageunder16', 'age16-24', 'age25-44', 'age45-64', 'age65over', 'gender_male', 'gender_female', 'gender_transgender,non-binary_or_two_spirit')
--- scripts:00-download_data.R
+++ scripts:00-download_data.R
@@ -1,18 +1,19 @@
 library(opendatatoronto)
 library(dplyr)
 
-# get package
+#Get package
 package <- show_package("ac77f532-f18b-427c-905c-4ae87ce69c93")
 package
 
-# get all resources for this package
+#Get all resources for this package
 resources <- list_package_resources("ac77f532-f18b-427c-905c-4ae87ce69c93")
 
-# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+#Identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
 datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
 
-# load the first datastore resource as a sample
+#Load the first datastore resource as a sample
 data <- filter(datastore_resources, row_number()==1) %>% get_resource()
 data
 
+#Name dataset
 write_csv(x = data, file = "inputs/rawdata.csv")
