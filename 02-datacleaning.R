b878688dd6b2379f7f81590f58cd4d7d37beea8f
rutykorotaev@mail.utoronto.ca <ruty.korotaiev@mail.utoronto.ca>
2023-02-03 21:33:27 -0500

    Updated code

--- 02-datacleaning.R
+++ 02-datacleaning.R
@@ -13,12 +13,9 @@ Demographic_Data <-
   filter(population_group == "Refugees") %>%
   select(`date(mmm-yy)`, 'ageunder16', 'age16-24', 'age25-44', 'age45-64', 'age65over', 'gender_male', 'gender_female', 'gender_transgender,non-binary_or_two_spirit')
 
-#Filter to create percentage of how many refugees are male, female, non-binary
+#Filter to create percentage of how many refugees are male, female, non-binary per year
 Refugees_total <-Demographic_Data$gender_male + Demographic_Data $gender_female + Demographic_Data$`gender_transgender,non-binary_or_two_spirit`
-Demographic_Data1 <- cbind(Demographic_Data,Refugees_total)
-
+dates <-Demographic_Data$`date(mmm-yy)`
 percentage_male <- Demographic_Data1$gender_male/Demographic_Data1$Refugees_total
-
 percentage_female <- Demographic_Data1$gender_female/Demographic_Data1$Refugees_total
-
 percentage_nonbinary <- Demographic_Data$`gender_transgender,non-binary_or_two_spirit`/Demographic_Data1$Refugees_total
