05356f753b8ad382b9f9d6c5dcc97b86db0ff6f9
rutykorotaev@mail.utoronto.ca <ruty.korotaiev@mail.utoronto.ca>
2023-02-05 23:33:17 -0500

    Cleaned data to create two separate datasets for housing information and demographic data

--- 02-datacleaning.R
+++ 02-datacleaning.R
@@ -8,14 +8,9 @@ Housing_Status <-
   select(date.mmm.yy., actively_homeless, newly_identified, moved_to_housing, became_inactive, returned_from_housing)
 
 #Filter data by gender and age of refugees
-Demographic_Data1 <-
-  data |>
+Demographic_Data <-
+  data1 |>
   filter(population_group == "Refugees") %>%
-  select(`date(mmm-yy)`, 'ageunder16', 'age16-24', 'age25-44', 'age45-64', 'age65over', 'gender_male', 'gender_female', 'gender_transgender,non-binary_or_two_spirit')
+  select(date.mmm.yy., ageunder16, age16-24, age25-44, age45-64, age65over, gender_male, gender_female, gender_transgender, non-binary_or_two_spirit)
 
-#Filter to create percentage of how many refugees are male, female, non-binary per year
-Refugees_total <- Demographic_Data$gender_male + Demographic_Data$gender_female + Demographic_Data$`gender_transgender,non-binary_or_two_spirit`
-dates <- Demographic_Data$`date(mmm-yy)`
-percentage_male <- Demographic_Data1$gender_male/Demographic_Data1$Refugees_total
-percentage_female <- Demographic_Data1$gender_female/Demographic_Data1$Refugees_total
-percentage_nonbinary <- Demographic_Data$`gender_transgender,non-binary_or_two_spirit`/Demographic_Data1$Refugees_total
+
