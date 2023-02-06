1f6af29698e096077223baa7d2a03f336933010d
rutykorotaev@mail.utoronto.ca <ruty.korotaiev@mail.utoronto.ca>
2023-02-05 23:59:38 -0500

    Graphed data simulation

--- scripts:01-simulation.R
+++ scripts:01-simulation.R
@@ -5,7 +5,13 @@ library(tidyverse)
 #Simulate dataset by month and number of refugees
 tibble(
   month = 1:12, 
-  number_homeless_refugees = runif(n = 12, min = 100, max = 1000)
-)
+  number_homeless_refugees = runif(n = 12, min = 100, max = 1000))
+
+#Graph simulated data
 df <- data.frame(Months = 1:12, Refugees = number_homeless_refugees)
+number_homeless_refugees |>
+  ggplot(mapping = aes(x = month, y = number_homeless_refugees)) +
+  geom_point()+
+  geom_line()
+
 
